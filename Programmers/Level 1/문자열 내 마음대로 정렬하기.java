d01a7dee03cbe5d79d3b35a52827458b7f6657c7
09^_^ <87968927+haaland09009@users.noreply.github.com>
2023-11-28 23:32:44 +0900

    문자열

---
+++ Programmers/Level 1/문자열 내 마음대로 정렬하기.java
@@ -0,0 +1,20 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12915
+import java.util.*;
+class Solution {
+    public String[] solution(String[] strings, int n) {
+        String[] answer = new String[strings.length];
+        
+        for(int i=0; i<strings.length; i++) {
+            strings[i] = strings[i].charAt(n) + strings[i];
+        }
+        
+        Arrays.sort(strings);
+ 
+        for(int i=0; i<strings.length; i++) {
+            answer[i] = strings[i].substring(1);
+        }
+        
+        
+        return answer;
+    }
+}
