6377a32574610fcbabbe07171b343494fa201cfd
09^_^ <87968927+haaland09009@users.noreply.github.com>
2023-11-28 19:34:38 +0900

    문자열

---
+++ Programmers/Level 1/숫자 문자열과 영단어.java
@@ -0,0 +1,13 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/81301
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        String[] arr = {"zero","one","two","three","four","five","six","seven","eight","nine"};
+        
+        for(int i=0; i<arr.length; i++) {
+            s = s.replaceAll(arr[i], Integer.toString(i));
+        }
+        
+        return Integer.parseInt(s);
+    }
+}
