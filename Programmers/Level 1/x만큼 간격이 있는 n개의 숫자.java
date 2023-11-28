a66cf9049c991eb79f043ab0837a90da38654fb7
09^_^ <87968927+haaland09009@users.noreply.github.com>
2023-11-28 16:46:12 +0900

    x만큼 간격이 있는 n개의 숫자.java

---
+++ Programmers/Level 1/x만큼 간격이 있는 n개의 숫자.java
@@ -0,0 +1,20 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12954
+import java.util.*;
+class Solution {
+    public long[] solution(int x, int n) {
+        long[] answer = {};
+        
+        List<Long> list = new ArrayList<>();
+        
+        for (int i=1; i<=n; i++) {
+            list.add((long) x*i);
+        }
+        
+        answer = new long[list.size()];
+        for(int i=0; i<list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        
+        return answer;
+    }
+}
