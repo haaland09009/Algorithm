0e5a2b3684140e889f43d0b6db0c68c948e3bd0a
09^_^ <87968927+haaland09009@users.noreply.github.com>
2023-11-28 19:04:46 +0900

    진법 변환

---
+++ Programmers/Level 1/3진법 뒤집기.java
@@ -0,0 +1,23 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/68935
+
+import java.util.*;
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        String str = "";
+        while (n != 0) {
+            str += String.valueOf(n % 3);
+            n = n / 3;
+        }
+        
+        String[] arr = str.split("");
+
+        for(int i=0; i<arr.length; i++) {
+            answer += Integer.parseInt(arr[i]) * (int)Math.pow(3, arr.length-1-i);
+        }
+        
+
+        return answer;
+    }
+}
