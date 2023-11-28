47573347b362933c3b0825cccfd49d5abbd2186d
09^_^ <87968927+haaland09009@users.noreply.github.com>
2023-11-28 17:59:13 +0900

    X

---
+++ Programmers/Level 1/문자열 다루기 기본.java
@@ -0,0 +1,21 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12918%EF%BB%BF
+class Solution {
+    public boolean solution(String s) {
+        boolean answer = true;
+        int length = s.length();
+        
+        if ((length != 4) && (length != 6)) {
+            answer = false;
+        }
+        
+        for(int i=0; i<s.length(); i++) {
+            if(Character.isLetter(s.charAt(i))) {
+                answer = false;
+                break;
+            }
+        }
+        
+        
+        return answer;
+    }
+}
