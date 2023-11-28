a15d3dccff57cb523b5b769b5a00d445eb8930fc
09^_^ <87968927+haaland09009@users.noreply.github.com>
2023-11-28 17:17:11 +0900

    문자, 문자열

---
+++ Programmers/Level 1/핸드폰 번호 가리기.java
@@ -0,0 +1,35 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12948
+// 1. 더 빠른 코드
+class Solution {
+    public String solution(String phone_number) {
+        String answer = "";
+        
+        int length = phone_number.length();
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<length-4; i++) {
+            sb.append('*');
+        }
+        
+        for(int i=length-4; i<=length-1; i++) {
+            sb.append(phone_number.charAt(i));
+        }
+   
+        return sb.toString();
+    }
+}
+
+////////////////////////////////////////////////
+// 2.
+class Solution {
+    public String solution(String phone_number) {
+        String answer = "";
+        
+        char[] ch = phone_number.toCharArray();
+        for(int i=0; i<ch.length-4; i++) {
+            ch[i] = '*';
+        }
+        
+        return String.valueOf(ch);
+    }
+}
