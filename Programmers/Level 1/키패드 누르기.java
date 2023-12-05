74bb1f5b5cedb3091694cc1110cbdd059f72965c
09^_^ <87968927+haaland09009@users.noreply.github.com>
2023-12-05 16:34:37 +0900

    배열

--- Programmers/Level 1/키패드 누르기.java
+++ Programmers/Level 1/키패드 누르기.java
@@ -41,3 +41,61 @@ class Solution {
         return answer;
     }
 }
+
+
+
+
+//////////////////////////////////////////////////////////////
+// 다른 풀이
+import java.util.*;
+class Solution {
+    int[][] pad = {
+        {3,1}, // 0
+        {0,0}, // 1
+        {0,1},
+        {0,2},
+        {1,0},
+        {1,1},
+        {1,2},
+        {2,0},
+        {2,1},
+        {2,2} // 9
+     };
+    public String solution(int[] numbers, String hand) {
+        String answer = "";
+        
+        int[] leftPos = {3,0};
+        int[] rightPos = {3,2};
+        
+        for (int num : numbers) {
+            if (num == 1 || num == 4 || num == 7) {
+                answer += "L";
+                leftPos = pad[num];
+            } else if (num == 3 || num == 6 || num == 9) {
+                answer += "R";
+                rightPos = pad[num];
+            } else {
+                if (getDist(leftPos, num) < getDist(rightPos, num)) {
+                    answer += "L";
+                    leftPos = pad[num];
+                } else if (getDist(leftPos, num) > getDist(rightPos, num)) {
+                    answer += "R";
+                    rightPos = pad[num];
+                } else {
+                    if (hand.equals("left")) {
+                        answer += "L";
+                        leftPos = pad[num];                        
+                    } else {
+                        answer += "R";
+                        rightPos = pad[num];                        
+                    }
+                }
+            }
+        }
+        
+        return answer;
+    }
+    private int getDist(int[] pos, int num) {
+        return Math.abs(pos[0] - pad[num][0]) + Math.abs(pos[1] - pad[num][1]);
+    }
+}
