4ece984fcd2582c992261de385e5e70046513074
09^_^ <87968927+haaland09009@users.noreply.github.com>
2023-12-06 23:50:12 +0900

    수학

--- Programmers/Level 1/덧칠하기.java
+++ Programmers/Level 1/덧칠하기.java
@@ -1,16 +1,18 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/161989
 class Solution {
     public int solution(int n, int m, int[] section) {
-        int answer = 1;
-        int prev = section[0];
+        int answer = 0;
         
-        for (int sec : section) {
-            if (sec - prev >= m) {
+        // 처음 페인트를 칠해야 하는 구역을 index로 설정
+        int idx = 0;
+
+        // 요소가 현재 idx 보다 크다면 아직 칠하지 않은 것이므로 1 증가시키기      
+        for (int s : section) {
+            if (s > idx) {
                 answer++;
-                prev = sec;
+                idx = s + m - 1; 
             } 
         }
-        
         
         return answer;
     }
