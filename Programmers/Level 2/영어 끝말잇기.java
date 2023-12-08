c1c2e6dd2f3385383f2574b35dd17e49ffe73025
09^_^ <87968927+haaland09009@users.noreply.github.com>
2023-12-09 01:11:51 +0900

    리스트

--- Programmers/Level 2/영어 끝말잇기.java
+++ Programmers/Level 2/영어 끝말잇기.java
@@ -4,7 +4,7 @@ class Solution {
     public int[] solution(int n, String[] words) {
         int[] answer = new int[2];
         List<String> list = new ArrayList<>();
-        list.add(words[0]);
+        list.add(words[0]); 
 
         for(int i=1; i<words.length; i++) {
             if (words[i].charAt(0) != words[i-1].charAt(words[i-1].length()-1)) {
