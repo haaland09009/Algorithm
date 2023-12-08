394bc5a0864634ccbaee6d5cb757d4cddb841082
09^_^ <87968927+haaland09009@users.noreply.github.com>
2023-12-09 01:14:40 +0900

    반복문, 조건문

---
+++ Programmers/Level 2/숫자의 표현.java
@@ -0,0 +1,21 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12924
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        int temp;
+        for(int i=1; i<=n; i++) {
+            temp = 0;
+            for(int j=i; j<=n; j++) {
+                temp += j;
+                if (temp == n) {
+                    answer++;
+                    break;
+                } else if (temp > n) {
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
