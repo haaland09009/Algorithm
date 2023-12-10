c607fe622dc8d63ba124ac846c81fde3dc1dc6fd
09^_^ <87968927+haaland09009@users.noreply.github.com>
2023-12-11 01:15:51 +0900

    문자열, 리스트

--- Programmers/Level 2/스킬트리.java
+++ Programmers/Level 2/스킬트리.java
@@ -1,30 +1,31 @@
-// 처음 해결한 풀이
+// https://school.programmers.co.kr/learn/courses/30/lessons/49993
 import java.util.*;
 class Solution {
     public int solution(String skill, String[] skill_trees) {
         int answer = 0;
         
         String[] skill_list = skill.split("");
-
-        for (int i=0; i<skill_trees.length; i++) {
+        
+        for (String skill_tree : skill_trees) {
+            boolean result = true;
             List<String> list = new ArrayList<>();
             for (String s : skill_list) {
                 list.add(s);
             }
-            boolean result = true;
-            for (int j=0; j<skill_trees[i].length(); j++) {
-                String ch = skill_trees[i].substring(j, j+1);
-                if (skill.contains(ch) && !ch.equals(list.get(0))) {
+            
+            for (int i=0; i<skill_tree.length(); i++) {
+                String sk = skill_tree.substring(i, i+1);
+                if (skill.contains(sk) && !sk.equals(list.get(0))) {
                     result = false;
                     break;
-                } else if (skill.contains(ch) && ch.equals(list.get(0))) {
+                } else if (skill.contains(sk) && sk.equals(list.get(0))) {
                     list.remove(0);
                 }
             }
-            if (result) 
+            
+            if (result)
                 answer++;
         }
-        
         
         return answer;
     }
