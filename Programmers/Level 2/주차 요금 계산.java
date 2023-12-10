b52cf1201f8ed55ef36772f7c8d4328eaf5dcc6d
09^_^ <87968927+haaland09009@users.noreply.github.com>
2023-12-10 17:27:32 +0900

    문자열, map, 리스트

---
+++ Programmers/Level 2/주차 요금 계산.java
@@ -0,0 +1,64 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/92341
+import java.util.*;
+class Solution {
+    public int[] solution(int[] fees, String[] records) {
+        int[] answer = {};
+        
+        Map<String, List<String>> map = new HashMap<>();
+        for (String r : records) {
+            String time = r.split(" ")[0]; String car = r.split(" ")[1];
+            if (!map.containsKey(car)) {
+                List<String> list = new ArrayList<>();
+                list.add(time);
+                map.put(car, list);
+            } else {
+                map.get(car).add(time);
+            }
+        }
+        
+        // 입차된 이후에 출차된 내역이 없을 경우 23:59에 출차된 것으로 간주한다.
+        for (String key : map.keySet()) {
+            if (map.get(key).size() % 2 != 0) {
+                map.get(key).add("23:59");
+            }
+        }
+        // 기본 요금: result <= 기본 시간 -> 5000
+        // 초과 요금 : 기본금 5000 +  Math.ceil((result - 기본 시간) / 단위 시간) x 단위 요금
+        // fees : [기본 시간, 기본 요금, 단위 시간, 단위 요금]        
+        
+        Map<String, Integer> result = new HashMap<>();
+        for (String key : map.keySet()) {
+            List<String> list = map.get(key);
+            int inTime = 0;  int outTime = 0;
+            // total_fees : 총 요금
+            int total_fees = 0; 
+            for (int i=0; i<list.size(); i+=2) {
+                String outHour = list.get(i+1).split(":")[0];
+                String outMinute = list.get(i+1).split(":")[1];
+                String inHour = list.get(i).split(":")[0];
+                String inMinute = list.get(i).split(":")[1];
+                
+                outTime += Integer.parseInt(outHour) * 60 + Integer.parseInt(outMinute);
+                inTime += Integer.parseInt(inHour) * 60 + Integer.parseInt(inMinute);    
+            }
+            int time = outTime - inTime;
+            if (time <= fees[0]) {
+                total_fees = fees[1];
+            } else {
+                total_fees = fees[1] + (int) Math.ceil((double) (time - fees[0]) / fees[2]) * fees[3];
+            }
+            result.put(key, total_fees);
+        }
+        
+        
+        List<String> carNumber = new ArrayList<>(map.keySet());
+        Collections.sort(carNumber);
+        
+        answer = new int[carNumber.size()];
+        for (int i=0; i<carNumber.size(); i++) {
+            answer[i] = result.get(carNumber.get(i));
+        }
+          
+        return answer;
+    }
+}
