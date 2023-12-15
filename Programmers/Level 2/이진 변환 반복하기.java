31379ae40e1abb750f107d6e5ccff4503ed5aa32
09^_^ <87968927+haaland09009@users.noreply.github.com>
2023-12-15 16:06:25 +0900

    이진 변환

---
+++ Programmers/Level 2/이진 변환 반복하기.java
@@ -0,0 +1,20 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/70129
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = new int[2];
+
+        while (!s.equals("1")) {
+            int cnt = 0;
+            for (int i=0; i<s.length(); i++) {
+                if (s.substring(i, i+1).equals("1"))
+                    cnt++;
+            }
+            answer[1] += s.length() - cnt;
+            answer[0]++;
+            s = Integer.toBinaryString(cnt);
+        }
+        
+        
+        return answer;
+    }
+}
