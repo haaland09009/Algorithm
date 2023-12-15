666ec801cbcf3e3194f39ded3f29bccf0dabc584
09^_^ <87968927+haaland09009@users.noreply.github.com>
2023-12-15 16:06:41 +0900

    진법 변환

--- Programmers/Level 2/이진 변환 반복하기.java
+++ Programmers/Level 2/이진 변환 반복하기.java
@@ -2,7 +2,7 @@
 class Solution {
     public int[] solution(String s) {
         int[] answer = new int[2];
-
+ 
         while (!s.equals("1")) {
             int cnt = 0;
             for (int i=0; i<s.length(); i++) {
