871d56a166de07df070982eacdda2db1a0ac2559
09^_^ <87968927+haaland09009@users.noreply.github.com>
2023-12-19 15:11:25 +0900

    진법 변환

---
+++ Programmers/Level 2/[3차] n진수 게임.java
@@ -0,0 +1,20 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/17687
+import java.util.*;
+class Solution {
+    public String solution(int n, int t, int m, int p) {
+        
+        StringBuilder convert = new StringBuilder();
+        StringBuilder answer = new StringBuilder();
+    
+        for (int i=0; i<=t*m; i++) {
+            convert.append(Integer.toString(i, n));
+        }
+        
+        for (int i=p-1; i<t*m; i+=m) {
+            answer.append(convert.substring(i, i+1));
+        }
+    
+        
+        return answer.toString().toUpperCase();
+    }
+}
