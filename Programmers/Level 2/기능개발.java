230c731eb322ac63cd787234c06fbb0f2ee2b99e
09^_^ <87968927+haaland09009@users.noreply.github.com>
2023-12-19 15:52:22 +0900

    queue

--- Programmers/Level 2/기능개발.java
+++ Programmers/Level 2/기능개발.java
@@ -4,33 +4,37 @@ class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         int[] answer = {};
         
-        int days = 1;
-        Queue<Integer> progressQueue = new LinkedList<>();
-        for (int p : progresses) 
-            progressQueue.add(p);
-
-        Queue<Integer> speedQueue = new LinkedList<>();
-        for (int s : speeds) 
-            speedQueue.add(s);        
+        Queue<Integer> pq = new LinkedList<>();
+        for (int p : progresses) {
+            pq.add(p);
+        }
         
+        Queue<Integer> sq = new LinkedList<>();
+        for (int s : speeds) {
+            sq.add(s);
+        }        
+        
+        int days = 1;
         int count = 0;
+        
         List<Integer> list = new ArrayList<>();
-        while (progressQueue.size() > 0) {
-            int job = progressQueue.peek();
-            int speed = speedQueue.peek();
+        while (pq.size() > 0) {
+            int job = pq.peek();
+            int speed = sq.peek();
             if (job + days * speed >= 100) {
-                progressQueue.poll();
-                speedQueue.poll();
+                pq.poll();
+                sq.poll();
                 count++;
             } else {
                 if (count > 0) {
                     list.add(count);
                     count = 0;
                 } else {
-                    days += 1;
+                    days++;
                 }
             }
         }
+        
         if (count > 0) {
             list.add(count);
         }
@@ -39,7 +43,8 @@ class Solution {
         for (int i=0; i<list.size(); i++) {
             answer[i] = list.get(i);
         }
-
+        
+        
         return answer;
     }
 }
