6945516ea22e36bf0766ec6704fb2bb07e9fe8da
09^_^ <87968927+haaland09009@users.noreply.github.com>
2023-12-21 18:49:24 +0900

    [Silver 1] 단지번호붙이기

--- Baekjoon/BFS DFS/단지번호붙이기/Main.java
+++ Baekjoon/BFS DFS/단지번호붙이기/Main.java
@@ -1,7 +1,7 @@
 //https://www.acmicpc.net/problem/2667
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.InputStreamReader; 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
