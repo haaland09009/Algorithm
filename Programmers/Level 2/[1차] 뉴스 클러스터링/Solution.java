d0eb140ed9185690373fb02271bb3fe5fb7829f5
09^_^ <87968927+haaland09009@users.noreply.github.com>
2023-12-23 01:01:57 +0900

    20231223

---
+++ Programmers/Level 2/[1차] 뉴스 클러스터링/Solution.java
@@ -0,0 +1,61 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/17677
+import java.util.*;
+class Solution {
+    public int solution(String str1, String str2) {
+        int answer = 0;
+        
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+        
+        ArrayList<String> list1 = new ArrayList<>();
+        ArrayList<String> list2 = new ArrayList<>();
+        
+        for (int i=0; i<str1.length()-1; i++) {
+            char a = str1.charAt(i);
+            char b = str1.charAt(i+1);
+            
+            if (Character.isLetter(a) && Character.isLetter(b)) {
+                String str = Character.toString(a) + Character.toString(b);
+                list1.add(str);
+            }
+        }
+        
+        for (int i=0; i<str2.length()-1; i++) {
+            char a = str2.charAt(i);
+            char b = str2.charAt(i+1);
+            
+            if (Character.isLetter(a) && Character.isLetter(b)) {
+                String str = Character.toString(a) + Character.toString(b);
+                list2.add(str);
+            }
+        }  
+        Collections.sort(list1);
+        Collections.sort(list2);        
+
+        ArrayList<String> union = new ArrayList<>();
+        ArrayList<String> inter = new ArrayList<>();
+        
+        for (String s : list1) {
+            if (list2.remove(s)) {
+                inter.add(s);
+            }
+            union.add(s);
+        }
+        
+        for (String s : list2) {
+            union.add(s);
+        }
+        
+        double a = inter.size();
+        double b = union.size();
+        
+        double jakard = 0;
+        if (b == 0) {
+            jakard = 1;
+        } else {
+            jakard = a / b;
+        }
+
+        return (int) (jakard * 65536);
+    }
+}
