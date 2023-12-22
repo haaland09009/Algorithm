1815790200efdf9c289fd74055c7c3b240d6cf5f
09^_^ <87968927+haaland09009@users.noreply.github.com>
2023-12-23 03:10:46 +0900

    20231223

---
+++ Programmers/Level 2/주식가격/Solution.java
@@ -0,0 +1,19 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/42584
+import java.util.*;
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+
+        for (int i=0; i<prices.length; i++) {
+            int time = 0;
+            for (int j=i+1; j<prices.length; j++) {
+                time++;
+                if (prices[i] > prices[j]) 
+                    break;  
+            }
+            answer[i] = time;
+        }
+    
+        return answer;
+    }
+}
