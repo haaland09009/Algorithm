f7e235a7d4fe7adc0a1cba67387ae9623a9c7693
09^_^ <87968927+haaland09009@users.noreply.github.com>
2023-12-25 23:48:41 +0900

    [Bronze 1] 단어 공부

--- Baekjoon/문자열/단어 공부/Main.java
+++ Baekjoon/문자열/단어 공부/Main.java
@@ -2,40 +2,74 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap; 
-import java.util.Map;
-
 public class Main {
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String word = br.readLine().toUpperCase();
 		
-		Map<Character, Integer> map = new HashMap<>();
-		int maxValue = 0;
+		int[] arr = new int[26];
+		for (int i=0; i<word.length(); i++) 
+			arr[word.charAt(i) - 'A']++;
 		
-		for (char letter : word.toCharArray()) {
-			map.put(letter, map.getOrDefault(letter, 0)+1);
-			maxValue = Math.max(maxValue, map.get(letter));
-		}
+		int maxValue = -1;
+		char answer = '?';
 		
-		int cnt = 0;
-		char answer = 0;
-		
-		for (char key : map.keySet()) {
-			if (map.get(key) == maxValue) {
-				cnt++;
-				answer = key;
+		for (int i=0; i<arr.length; i++) {
+			if (arr[i] > maxValue) {
+				maxValue = arr[i];
+				answer = (char) (i+65);
+			} else if (arr[i] == maxValue) {
+				answer = '?';
 			}
 		}
 		
-		if (cnt >= 2) {
-			System.out.println("?");
-		} else {
-			System.out.println(answer);
-		}
-		
-		
-		
+		System.out.println(answer);
 	}
 }
+
+
+
+
+/////////////////////////////////////////////////////////
+// 처음 해결한 풀이
+// import java.io.BufferedReader;
+// import java.io.IOException;
+// import java.io.InputStreamReader;
+// import java.util.HashMap; 
+// import java.util.Map;
+
+// public class Main {
+// 	public static void main(String[] args) throws IOException {
+		
+// 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+// 		String word = br.readLine().toUpperCase();
+		
+// 		Map<Character, Integer> map = new HashMap<>();
+// 		int maxValue = 0;
+		
+// 		for (char letter : word.toCharArray()) {
+// 			map.put(letter, map.getOrDefault(letter, 0)+1);
+// 			maxValue = Math.max(maxValue, map.get(letter));
+// 		}
+		
+// 		int cnt = 0;
+// 		char answer = 0;
+		
+// 		for (char key : map.keySet()) {
+// 			if (map.get(key) == maxValue) {
+// 				cnt++;
+// 				answer = key;
+// 			}
+// 		}
+		
+// 		if (cnt >= 2) {
+// 			System.out.println("?");
+// 		} else {
+// 			System.out.println(answer);
+// 		}
+		
+		
+		
+// 	}
+// }
