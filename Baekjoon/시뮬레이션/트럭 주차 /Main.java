51c6bb1063e8b0968744117e78c45b20608217ba
09^_^ <87968927+haaland09009@users.noreply.github.com>
2024-01-08 19:46:52 +0900

    [Bronze 2] 트럭 주차

--- Baekjoon/시뮬레이션/트럭 주차 /Main.java
+++ Baekjoon/시뮬레이션/트럭 주차 /Main.java
@@ -1,7 +1,7 @@
 //https://www.acmicpc.net/problem/2979
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.InputStreamReader; 
 import java.util.StringTokenizer;
 public class Main {
 	public static void main(String[] args) throws IOException {
