0d93e7281262a56d595063747095cf07c3a310c6
09^_^ <87968927+haaland09009@users.noreply.github.com>
2024-01-15 15:33:09 +0900

    20240115

--- Programmers/Level 2/N개의 최소공배수/Solution.java
+++ Programmers/Level 2/N개의 최소공배수/Solution.java
@@ -2,21 +2,24 @@
 import java.util.*;
 class Solution {
     public int solution(int[] arr) {
- 
-        for (int i=1; i<arr.length; i++) {
-            arr[i] = lcm(arr[i], arr[i-1]);
-        }
+        int answer = 0;
+        Arrays.sort(arr);
         
+        for (int i=1; i<arr.length; i++) {
+            arr[i] = lcm(arr[i-1], arr[i]);
+        }
+     
         return arr[arr.length-1];
     }
     
-    public static int gcd(int a, int b) {
-        if (b == 0) 
+    private int gcd(int a, int b) {
+        if (b % a == 0)
             return a;
-        return gcd(b, a % b);    
+        return gcd(b % a, a);
     }
     
-    public static int lcm(int a, int b) {
-        return a * b / gcd(a, b);
+    private int lcm(int a, int b) {
+         return a * b / gcd(a, b);
+        
     }
 }
