cbdeced76992b1366b4290e3a543c326e31dbea8
09^_^ <87968927+haaland09009@users.noreply.github.com>
2024-01-16 20:04:16 +0900

    20240116

--- Programmers/Level 2/N개의 최소공배수/Solution.java
+++ Programmers/Level 2/N개의 최소공배수/Solution.java
@@ -3,7 +3,7 @@ import java.util.*;
 class Solution {
     public int solution(int[] arr) {
         int answer = 0;
-        Arrays.sort(arr);
+        Arrays.sort(arr); 
         
         for (int i=1; i<arr.length; i++) {
             arr[i] = lcm(arr[i-1], arr[i]);
