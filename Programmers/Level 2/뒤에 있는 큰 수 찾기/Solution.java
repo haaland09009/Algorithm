709e424316364241d7618d5a7618814e27774543
09^_^ <87968927+haaland09009@users.noreply.github.com>
2024-01-17 22:57:58 +0900

    20240117

--- Programmers/Level 2/뒤에 있는 큰 수 찾기/Solution.java
+++ Programmers/Level 2/뒤에 있는 큰 수 찾기/Solution.java
@@ -4,20 +4,19 @@ class Solution {
     public int[] solution(int[] numbers) {
         int[] answer = new int[numbers.length];
         
-        Stack<Integer> stack = new Stack<>();
-        stack.push(0);
+        for (int i=0; i<numbers.length; i++) {
+            answer[i] = -1;
+        }
         
-        for (int i=1; i<numbers.length; i++) {
-            while (!stack.isEmpty() && numbers[stack.peek()] < numbers[i]) {
+        Stack<Integer> stack = new Stack<>();
+        
+        for (int i=0; i<numbers.length; i++) {     
+            while (!stack.isEmpty() && numbers[i] > numbers[stack.peek()]) {
                 answer[stack.pop()] = numbers[i];
             }
-            stack.push(i);
+            stack.push(i);       
         }
-        
-        while (!stack.isEmpty()) {
-            answer[stack.pop()] = -1;
-        }
-        
+    
         
         return answer;
     }
