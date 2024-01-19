b6851b428b30e7296a837cc8b6e8898f7c167e70
09^_^ <87968927+haaland09009@users.noreply.github.com>
2024-01-19 17:58:44 +0900

    20240119

--- Programmers/Level 1/문자열 다루기 기본/Solution.java
+++ Programmers/Level 1/문자열 다루기 기본/Solution.java
@@ -1,20 +1,17 @@
-// https://school.programmers.co.kr/learn/courses/30/lessons/12918%EF%BB%BF
+// https://school.programmers.co.kr/learn/courses/30/lessons/12918
 class Solution {
     public boolean solution(String s) {
         boolean answer = true;
-        int length = s.length();
-         
-        if ((length != 4) && (length != 6)) {
-            answer = false;
-        }
         
-        for(int i=0; i<s.length(); i++) {
-            if(Character.isLetter(s.charAt(i))) {
+        for (int i=0; i<s.length(); i++) {
+            if (Character.isLetter(s.charAt(i))) {
                 answer = false;
                 break;
             }
         }
         
+        if (s.length() != 4 && s.length() != 6) 
+            answer = false;
         
         return answer;
     }
