e1a8153381eb5ea2b1f8285196ad4332dec0ee93
09^_^ <87968927+haaland09009@users.noreply.github.com>
2024-01-23 23:02:06 +0900

    20240123

--- Programmers/Level 1/숫자 문자열과 영단어/Solution.java
+++ Programmers/Level 1/숫자 문자열과 영단어/Solution.java
@@ -11,3 +11,40 @@ class Solution {
         return Integer.parseInt(s);
     }
 }
+
+
+
+// import java.util.*;
+// class Solution {
+//     public int solution(String s) {
+     
+//         Map<String, String> map = new HashMap<>();
+//         map.put("zero", "0");
+//         map.put("one", "1");
+//         map.put("two", "2");
+//         map.put("three", "3");
+//         map.put("four", "4");
+//         map.put("five", "5");
+//         map.put("six", "6");
+//         map.put("seven", "7");
+//         map.put("eight", "8");
+//         map.put("nine", "9");
+        
+//         String result = "";
+//         String tmp = "";
+//         for (int i=0; i<s.length(); i++) {
+//             if (Character.isLetter(s.charAt(i))) {
+//                 tmp += s.substring(i, i+1);
+//                 if (map.containsKey(tmp)) {
+//                     result += map.get(tmp);
+//                     tmp = "";
+//                 }
+//             } else {
+//                 result += String.valueOf(s.charAt(i));
+//             }
+//         }
+        
+  
+//         return Integer.parseInt(result.toString());
+//     }
+// }
