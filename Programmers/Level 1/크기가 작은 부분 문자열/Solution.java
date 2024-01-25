110e1673f34659e726e7eb98bf3909d6f3a1b901
09^_^ <87968927+haaland09009@users.noreply.github.com>
2024-01-25 19:21:06 +0900

    20240125

--- Programmers/Level 1/크기가 작은 부분 문자열/Solution.java
+++ Programmers/Level 1/크기가 작은 부분 문자열/Solution.java
@@ -1,16 +1,15 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/147355
-import java.util.*;
 class Solution {
     public int solution(String t, String p) {
         int answer = 0;
+        
         int length = p.length();
-        Long value = Long.parseLong(p);
-         
-        for(int i=0; i<=t.length()-length; i++) {
-            Long num = Long.parseLong(t.substring(i, i+length));
-            if (num <= value) {
+        Long number = Long.parseLong(p);
+        
+        for (int i=0; i<=t.length()-length; i++) {
+            String str = t.substring(i, i+length);
+            if (Long.parseLong(str) <= number)
                 answer++;
-            }
         }
         
         return answer;
