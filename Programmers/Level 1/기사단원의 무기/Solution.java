114f2f5b885cd78be9c529a8ddd621a953f2a6d8
09^_^ <87968927+haaland09009@users.noreply.github.com>
2024-01-28 23:21:49 +0900

    20240128

--- Programmers/Level 1/기사단원의 무기/Solution.java
+++ Programmers/Level 1/기사단원의 무기/Solution.java
@@ -1,32 +1,56 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/136798
-import java.util.*;
 class Solution {
+
     public int solution(int number, int limit, int power) {
-        int answer = 0;
-        
-        for (int i=1; i<=number; i++) {
-            answer += getCountDivisor(i, limit, power);
+        int[] count = new int[number + 1];    
+        for (int i = 1; i <= number; i++) {
+            for (int j = 1; j <= number / i; j++) {
+                count[i * j]++;
+            }
         }
-        
+        int answer = 0;
+        for (int i = 1; i <= number; i++) {
+            if (count[i] > limit) {
+                answer += power;
+            } else {
+                answer += count[i];
+            }
+        }
         return answer;
     }
-    
-    
-    public int getCountDivisor(int number, int limit, int power) {
-        
-        int count = 0;
-        for (int i=1; i<(int)(Math.sqrt(number))+1; i++) {
-            if (number % i == 0) {
-                if (i == number / i) {
-                    count += 1;
-                } else {
-                    count += 2;
-                }
-            } 
-        }
-        if (count > limit) 
-            return power;
-        return count;
-    }
-    
 }
+
+
+
+
+// import java.util.*;
+// class Solution {
+//     public int solution(int number, int limit, int power) {
+//         int answer = 0;
+        
+//         for (int i=1; i<=number; i++) {
+//             answer += getCountDivisor(i, limit, power);
+//         }
+        
+//         return answer;
+//     }
+    
+    
+//     public int getCountDivisor(int number, int limit, int power) {
+        
+//         int count = 0;
+//         for (int i=1; i<(int)(Math.sqrt(number))+1; i++) {
+//             if (number % i == 0) {
+//                 if (i == number / i) {
+//                     count += 1;
+//                 } else {
+//                     count += 2;
+//                 }
+//             } 
+//         }
+//         if (count > limit) 
+//             return power;
+//         return count;
+//     }
+    
+// }
