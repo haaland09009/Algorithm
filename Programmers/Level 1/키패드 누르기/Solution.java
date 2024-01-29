e2275ab913d22d0ed39a2447952ad4d41a9906eb
09^_^ <87968927+haaland09009@users.noreply.github.com>
2024-01-29 16:36:11 +0900

    20240129

--- Programmers/Level 1/키패드 누르기/Solution.java
+++ Programmers/Level 1/키패드 누르기/Solution.java
@@ -1,101 +1,122 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/67256
 import java.util.*;
 class Solution {
+    int[][] pad = {
+        {3,1}, // 0
+        {0,0}, // 1
+        {0,1},
+        {0,2},
+        {1,0},
+        {1,1},
+        {1,2},
+        {2,0},
+        {2,1},
+        {2,2} // 9
+     };
     public String solution(int[] numbers, String hand) {
         String answer = "";
         
-        int left = 10;
-        int right = 12;
-      
+        int[] leftPos = {3,0};
+        int[] rightPos = {3,2};
+        
         for (int num : numbers) {
             if (num == 1 || num == 4 || num == 7) {
                 answer += "L";
-                left = num;
+                leftPos = pad[num];
             } else if (num == 3 || num == 6 || num == 9) {
                 answer += "R";
-                right = num;
+                rightPos = pad[num];
             } else {
-                if (num == 0) 
-                    num = 11;
-                int leftDist = Math.abs(num - left) / 3 + Math.abs(num - left) % 3;
-                int rightDist = Math.abs(num - right) / 3 + Math.abs(num - right) % 3;
-                
-                if (leftDist < rightDist) {
+                if (getDist(leftPos, num) < getDist(rightPos, num)) {
                     answer += "L";
-                    left = num;
-                } else if (leftDist > rightDist) {
+                    leftPos = pad[num];
+                } else if (getDist(leftPos, num) > getDist(rightPos, num)) {
                     answer += "R";
-                    right = num;
+                    rightPos = pad[num];
                 } else {
                     if (hand.equals("left")) {
                         answer += "L";
-                        left = num;
+                        leftPos = pad[num];                        
                     } else {
                         answer += "R";
-                        right = num;
+                        rightPos = pad[num];                        
                     }
                 }
             }
         }
-    
+        
         return answer;
+    }
+    private int getDist(int[] pos, int num) {
+        return Math.abs(pos[0] - pad[num][0]) + Math.abs(pos[1] - pad[num][1]);
     }
 }
 
-
-
-
-//////////////////////////////////////////////////////////////
-// 다른 풀이
 // import java.util.*;
 // class Solution {
-//     int[][] pad = {
-//         {3,1}, // 0
-//         {0,0}, // 1
-//         {0,1},
-//         {0,2},
-//         {1,0},
-//         {1,1},
-//         {1,2},
-//         {2,0},
-//         {2,1},
-//         {2,2} // 9
-//      };
 //     public String solution(int[] numbers, String hand) {
 //         String answer = "";
         
-//         int[] leftPos = {3,0};
-//         int[] rightPos = {3,2};
+//         Map<Integer, int[]> map = new HashMap<>();
+//         map.put(0, new int[]{3, 1});
+//         map.put(1, new int[]{0, 0}); map.put(2, new int[]{0, 1}); map.put(3, new int[]{0, 2});
+//         map.put(4, new int[]{1, 0}); map.put(5, new int[]{1, 1}); map.put(6, new int[]{1, 2});
+//         map.put(7, new int[]{2, 0}); map.put(8, new int[]{2, 1}); map.put(9, new int[]{2, 2});
+//         map.put(10, new int[]{3, 0}); map.put(11, new int[]{3, 2});
         
-//         for (int num : numbers) {
-//             if (num == 1 || num == 4 || num == 7) {
+//         int[] leftPos = map.get(10); // *
+//         int[] rightPos = map.get(11); // #
+//         // System.out.println(leftPos[0]);
+
+//         for (int number : numbers) {
+//             if (number == 1 || number == 4 || number == 7) {
 //                 answer += "L";
-//                 leftPos = pad[num];
-//             } else if (num == 3 || num == 6 || num == 9) {
+//                 leftPos[0] = map.get(number)[0];
+//                 leftPos[1] = map.get(number)[1];              
+//             } else if (number == 3 || number == 6 || number == 9) {
 //                 answer += "R";
-//                 rightPos = pad[num];
+//                 rightPos[0] = map.get(number)[0];
+//                 rightPos[1] = map.get(number)[1];
 //             } else {
-//                 if (getDist(leftPos, num) < getDist(rightPos, num)) {
-//                     answer += "L";
-//                     leftPos = pad[num];
-//                 } else if (getDist(leftPos, num) > getDist(rightPos, num)) {
+//                 int[] curPos = map.get(number);
+//                 int res = getDistance(leftPos, rightPos, curPos);
+//                 if (res == 1) {
+//                     rightPos[0] = map.get(number)[0];
+//                     rightPos[1] = map.get(number)[1];
 //                     answer += "R";
-//                     rightPos = pad[num];
+//                 } else if (res == -1) {
+//                     leftPos[0] = map.get(number)[0];
+//                     leftPos[1] = map.get(number)[1];
+//                     answer += "L";
 //                 } else {
 //                     if (hand.equals("left")) {
-//                         answer += "L";
-//                         leftPos = pad[num];                        
+//                         leftPos[0] = map.get(number)[0];
+//                         leftPos[1] = map.get(number)[1];
+//                         answer += "L";                        
 //                     } else {
+//                         rightPos[0] = map.get(number)[0];
+//                         rightPos[1] = map.get(number)[1];
 //                         answer += "R";
-//                         rightPos = pad[num];                        
 //                     }
 //                 }
+                
 //             }
 //         }
-        
 //         return answer;
 //     }
-//     private int getDist(int[] pos, int num) {
-//         return Math.abs(pos[0] - pad[num][0]) + Math.abs(pos[1] - pad[num][1]);
+    
+//     private int getDistance(int[] leftPos, int[] rightPos, int[] curPos) {
+//         int result = 0;
+//         int leftDistance = Math.abs(curPos[0] - leftPos[0]) + Math.abs(curPos[1] - leftPos[1]);
+//         int rightDistance = Math.abs(curPos[0] - rightPos[0]) + Math.abs(curPos[1] - rightPos[1]);
+        
+//         if (leftDistance > rightDistance) {
+//             result = 1;
+//         } else if (leftDistance < rightDistance) {
+//             result = -1;
+//         } else {
+//             result = 0;
+//         }
+//         return result;
 //     }
 // }
