5954f35c7bfc6ccaa1edd5930dbf3d0526e0a070
09^_^ <87968927+haaland09009@users.noreply.github.com>
2024-02-01 22:52:17 +0900

    20240201

--- Programmers/Level 2/미로 탈출/Solution.java
+++ Programmers/Level 2/미로 탈출/Solution.java
@@ -57,3 +57,68 @@ class Solution {
     
     
 }
+
+
+
+import java.util.*;
+class Solution {
+    public int solution(String[] maps) {
+        int answer = 0;
+            
+        int path1 = bfs('S', 'L', maps);
+        int path2 = bfs('L', 'E', maps);
+
+        if (path1 != -1 && path2 != -1) 
+            return path1 + path2;
+        else
+            return -1;
+     
+    }
+    
+//     private int bfs(char start, char end, String[] maps) {
+        
+//         Queue<int[]> queue = new LinkedList<>();
+        
+//         int[] dx = {0,0,-1,1};
+//         int[] dy = {-1,1,0,0};
+
+//         int m = maps.length;
+//         int n = maps[0].length();
+        
+//         int[][] visited = new int[m][n];
+        
+//         boolean flag = false;
+//         for (int i=0; i<m; i++) {
+//             for (int j=0; j<n; j++) {
+//                 if (maps[i].charAt(j) == start) {
+//                     queue.add(new int[]{i, j});
+//                     flag = true;
+//                     break;
+//                 }
+//             }
+//             if (flag)
+//                 break;
+//         }
+        
+//         while (!queue.isEmpty()) {
+//             int[] cur = queue.poll();
+//             int cx = cur[0];
+//             int cy = cur[1];
+//             if (maps[cx].charAt(cy) == end) {
+//                 return visited[cx][cy];
+//             }
+//             for (int i=0; i<4; i++) {
+//                 int nx = cx + dx[i];
+//                 int ny = cy + dy[i];
+//                 if (0 <= nx && nx < m && 0 <= ny && ny < n) {
+//                     if (maps[nx].charAt(ny) != 'X' && visited[nx][ny] == 0) {
+//                         visited[nx][ny] = visited[cx][cy] + 1;
+//                         queue.add(new int[]{nx, ny});
+//                     }
+//                 }
+//             }   
+//         }
+//       return -1;          
+//     }
+    
+// }
