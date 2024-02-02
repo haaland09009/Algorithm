b8032146cbdf1018bde4e7139b4cbb5780d1ab87
09^_^ <87968927+haaland09009@users.noreply.github.com>
2024-02-02 15:08:55 +0900

    20240202

--- Programmers/Level 1/바탕화면 정리/Solution.java
+++ Programmers/Level 1/바탕화면 정리/Solution.java
@@ -22,3 +22,34 @@ class Solution {
         return new int[]{top, left, bottom, right};
     }
 }
+
+
+
+// import java.util.*;
+// class Solution {
+//     public int[] solution(String[] wallpaper) {
+//         int[] answer = new int[4];
+                
+//         int min_x = wallpaper.length;   
+//         int max_x = 0;
+//         int min_y = wallpaper[0].length()-1;  
+//         int max_y = 0;
+        
+//         for (int i=0; i<wallpaper.length; i++) {
+//             for (int j=0; j<wallpaper[i].length(); j++) {
+//                 if (wallpaper[i].charAt(j) == '#') {
+//                     min_x = Math.min(min_x, i);
+//                     max_x = Math.max(max_x, i+1);
+//                     min_y = Math.min(min_y, j);
+//                     max_y = Math.max(max_y, j+1);
+//                 }
+//             }
+//         }
+        
+//         answer[0] = min_x; 
+//         answer[1] = min_y;
+//         answer[2] = max_x; 
+//         answer[3] = max_y;
+//         return answer;
+//     }
+// }
