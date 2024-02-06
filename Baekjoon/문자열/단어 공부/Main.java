96cf73c22df12d41a73d17918709a4472afb06a3
09^_^ <87968927+haaland09009@users.noreply.github.com>
2024-02-06 23:40:18 +0900

    20240206

--- Baekjoon/문자열/단어 공부/Main.java
+++ Baekjoon/문자열/단어 공부/Main.java
@@ -33,43 +33,39 @@ public class Main {
 
 /////////////////////////////////////////////////////////
 // 처음 해결한 풀이
-// import java.io.BufferedReader;
-// import java.io.IOException;
-// import java.io.InputStreamReader;
-// import java.util.HashMap; 
-// import java.util.Map;
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String word = br.readLine().toUpperCase();
 
-// public class Main {
-// 	public static void main(String[] args) throws IOException {
-		
-// 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-// 		String word = br.readLine().toUpperCase();
-		
-// 		Map<Character, Integer> map = new HashMap<>();
-// 		int maxValue = 0;
-		
-// 		for (char letter : word.toCharArray()) {
-// 			map.put(letter, map.getOrDefault(letter, 0)+1);
-// 			maxValue = Math.max(maxValue, map.get(letter));
-// 		}
-		
-// 		int cnt = 0;
-// 		char answer = 0;
-		
-// 		for (char key : map.keySet()) {
-// 			if (map.get(key) == maxValue) {
-// 				cnt++;
-// 				answer = key;
-// 			}
-// 		}
-		
-// 		if (cnt >= 2) {
-// 			System.out.println("?");
-// 		} else {
-// 			System.out.println(answer);
-// 		}
-		
-		
-		
-// 	}
-// }
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < word.length(); i++) {
+            char letter = word.charAt(i);
+            map.put(letter, map.getOrDefault(letter, 0) + 1);
+        }
+
+        int max = 0;
+        for (char key : map.keySet()) {
+            max = Math.max(max, map.get(key));
+        }
+
+        int cnt = 0;
+        List<Character> list = new ArrayList<>();
+        for (char key : map.keySet()) {
+            if (map.get(key) == max) {
+                list.add(key);
+                cnt++;
+            }
+        }
+
+        if (cnt >= 2) {
+            System.out.println("?");
+        } else {
+            System.out.println(list.get(0));
+        }
+
+    }
+}
+
