0aa96112561524cf0fbccf70fd318c35c007795f
09^_^ <87968927+haaland09009@users.noreply.github.com>
2024-02-11 17:12:50 +0900

    [Silver 4] 2+1 세일

---
+++ Baekjoon/그리디/2+1 세일/Main.java
@@ -0,0 +1,30 @@
+//https://www.acmicpc.net/problem/11508
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		Integer[] arr = new Integer[n];
+		for (int i=0; i<n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		Arrays.sort(arr, Collections.reverseOrder());
+		int answer = 0;
+		for (int i=0; i<n; i++) {
+			if (i % 3 != 2) {
+				answer += arr[i];
+			}
+		}
+		
+		System.out.println(answer);
+	}
+}
