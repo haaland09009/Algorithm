30c8f4a1b5e14805a3607d1b5614e50985860db2
09^_^ <87968927+haaland09009@users.noreply.github.com>
2024-03-23 17:12:08 +0900

    240323

--- Programmers/Level 2/JadenCase 문자열 만들기/Solution.java
+++ Programmers/Level 2/JadenCase 문자열 만들기/Solution.java
@@ -1,26 +1,22 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/12951
 class Solution {
     public String solution(String s) {
-
         s = s.toLowerCase();
-        
         StringBuilder sb = new StringBuilder();
-        int idx = 0;
         
+        int idx = 0;
         for (int i=0; i<s.length(); i++) {
             String word = s.substring(i, i+1);
             if (word.equals(" ")) {
                 idx = 0;
-                sb.append(word);
             } else {
-                if (idx == 0) 
-                    sb.append(word.toUpperCase());
-                else 
-                    sb.append(word);
+                if (idx == 0) {
+                    word = word.toUpperCase();
+                } 
                 idx++;
             }
+            sb.append(word);
         }
-        
         return sb.toString();
     }
 }
