c9ae2d42832946217b942ee5c2734084b3dd433a
09^_^ <87968927+haaland09009@users.noreply.github.com>
2024-03-30 18:36:24 +0900

    20240330

--- Programmers/Level 2/[3차] n진수 게임/Solution.java
+++ Programmers/Level 2/[3차] n진수 게임/Solution.java
@@ -18,3 +18,38 @@ class Solution {
         return answer.toString().toUpperCase();
     }
 }
+
+/* 
+class Solution {
+    public String solution(int n, int t, int m, int p) {
+        
+        StringBuilder str = new StringBuilder();
+        for (int i=0; i<=t*m; i++) {
+            str.append(convert(i, n));
+        }
+        
+        StringBuilder answer = new StringBuilder();
+        for (int i=p-1; i<=t*m; i+=m) {
+            answer.append(str.substring(i, i+1));
+            if (answer.toString().length() == t) {
+                break;
+            }
+        }
+        return answer.toString();
+    }
+    
+    
+    private String convert(int num, int n) {
+        String a = "0123456789ABCDEF";
+        String str = "";
+        if (num == 0) {
+            return "0";
+        }
+        while (num > 0) {
+            str = String.valueOf(a.charAt(num % n)) + str;
+            num = num / n;
+        }
+        return str;
+    }
+} 
+*/
