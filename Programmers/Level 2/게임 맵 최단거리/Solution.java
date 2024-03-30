d427824a3a61581b799fdd29b0113449ca85b7b1
09^_^ <87968927+haaland09009@users.noreply.github.com>
2024-03-30 16:07:04 +0900

    20240330

--- Programmers/Level 2/게임 맵 최단거리/Solution.java
+++ Programmers/Level 2/게임 맵 최단거리/Solution.java
@@ -1,44 +1,41 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/1844
 import java.util.*;
 class Solution {
-    static int[] dx = {0,0,-1,1};
-    static int[] dy = {-1,1,0,0};
+    int[] dx = {0,0,-1,1};
+    int[] dy = {-1,1,0,0};
+    int n, m;
     public int solution(int[][] maps) {
         int answer = 0;
         
-        int[][] visited = new int[maps.length][maps[0].length];
+        n = maps.length;
+        m = maps[0].length;
         
-        bfs(maps, visited);
-        answer = visited[maps.length-1][maps[0].length-1];
-        if (answer == 0) 
-            return -1;
-
+        answer = bfs(0, 0, maps);
+        
         return answer;
     }
     
-    public void bfs(int[][] maps, int[][] visited) {
-        int x = 0;
-        int y = 0;
-        visited[x][y] = 1;
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{x,y});
-        
-        while (!queue.isEmpty()) {
-            int[] cur = queue.poll();
+    private int bfs(int x, int y, int[][] maps) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{x, y});
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
             int cx = cur[0];
             int cy = cur[1];
+            if (cx == n-1 && cy == m-1) {
+                return maps[n-1][m-1];
+            }
             for (int i=0; i<4; i++) {
                 int nx = cx + dx[i];
                 int ny = cy + dy[i];
-                if (nx >= 0 && nx < maps.length && ny >= 0 && ny < maps[0].length) {
-                    if (maps[nx][ny] == 1 && visited[nx][ny] == 0) {
-                        visited[nx][ny] = visited[cx][cy] + 1;
-                        queue.add(new int[]{nx,ny});
+                if (0 <= nx && nx < n && 0 <= ny && ny < m) {
+                    if (maps[nx][ny] == 1) {
+                        maps[nx][ny] = maps[cx][cy] + 1;
+                        q.add(new int[]{nx, ny});
                     }
                 }
             }
         }
+        return -1;
     }
-    
-    
 }
