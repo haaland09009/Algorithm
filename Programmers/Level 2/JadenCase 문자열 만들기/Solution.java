2d72cc7f634fe288bc3a94066d53a3555e8cf40a
09^_^ <87968927+haaland09009@users.noreply.github.com>
2024-04-20 22:17:46 +0900

    20240420

--- Programmers/Level 2/JadenCase 문자열 만들기/Solution.java
+++ Programmers/Level 2/JadenCase 문자열 만들기/Solution.java
@@ -4,7 +4,7 @@ class Solution {
         s = s.toLowerCase();
         StringBuilder sb = new StringBuilder();
         
-        int idx = 0;
+        int idx = 0; 
         for (int i=0; i<s.length(); i++) {
             String word = s.substring(i, i+1);
             if (word.equals(" ")) {
