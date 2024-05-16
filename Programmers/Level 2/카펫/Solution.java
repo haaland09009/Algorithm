51a8fcf846f381ff05fab6cd2709281b510db0f4
09^_^ <87968927+haaland09009@users.noreply.github.com>
2024-05-16 10:40:29 +0900

    20240516

--- Programmers/Level 2/카펫/Solution.java
+++ Programmers/Level 2/카펫/Solution.java
@@ -1,4 +1,23 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/42842
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        
+        int sum = brown + yellow;
+        
+        for (int i=1; i<=sum; i++) {
+            int num = sum / i;
+            if ((num - 2) * (i - 2) == yellow) {
+                answer[0] = num;
+                answer[1] = i;
+                break;
+            }
+        }
+        return answer;
+    }
+}
+
+/*
 import java.util.*;
 class Solution {
     public int[] solution(int brown, int yellow) {
@@ -27,3 +46,4 @@ class Solution {
         return answer;
     }
 }
+*/
