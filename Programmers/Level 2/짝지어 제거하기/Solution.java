a8ebf4750d57371f4f0611d03f08163e0578e47b
09^_^ <87968927+haaland09009@users.noreply.github.com>
2024-05-16 10:29:26 +0900

    20240516

--- Programmers/Level 2/짝지어 제거하기/Solution.java
+++ Programmers/Level 2/짝지어 제거하기/Solution.java
@@ -5,21 +5,18 @@ class Solution
 {
     public int solution(String s)
     {
-        int answer = -1;
+        
         Stack<Character> stack = new Stack<>();
         
         for (int i=0; i<s.length(); i++) {
-            if (stack.size() == 0) {
-                stack.push(s.charAt(i));
-            } else if (stack.peek() == s.charAt(i)) {
+            if (stack.size() >= 1 && stack.peek() == s.charAt(i)) {
                 stack.pop();
             } else {
                 stack.push(s.charAt(i));
             }
         }
         
-        
+     return stack.isEmpty() ? 1 : 0;
 
-        return stack.isEmpty() ? 1 : 0;
     }
 }
