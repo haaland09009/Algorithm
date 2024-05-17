877bcb9f6c945d848518be24c76963e436b1349a
09^_^ <87968927+haaland09009@users.noreply.github.com>
2024-05-17 16:34:23 +0900

    20240517

--- Programmers/Level 1/대충 만든 자판/Solution.java
+++ Programmers/Level 1/대충 만든 자판/Solution.java
@@ -33,3 +33,46 @@ class Solution {
         return answer;
     }
 }
+
+
+
+
+/*
+import java.util.*;
+class Solution {
+    public int[] solution(String[] keymap, String[] targets) {
+        int[] answer = new int[targets.length];
+        
+        Map<Character, Integer> map = new HashMap<>();
+        
+        for (String key : keymap) {
+           for (int i=0; i<key.length(); i++) {
+               char c = key.charAt(i);
+               if (map.get(c) != null && i+1 < map.get(c)) {
+                   map.put(c, Math.min(i+1, map.get(c)));
+               } else if (map.get(c) == null) {
+                   map.put(key.charAt(i), i + 1);
+               }
+           }
+        }
+        
+    
+        for (int i=0; i<targets.length; i++) {
+            int count = 0;
+            for (int j=0; j<targets[i].length(); j++) {
+                char c = targets[i].charAt(j);
+                if (map.get(c) != null) {
+                    count += map.get(c);
+                } else {
+                    count = -1;
+                    break;
+                }
+            }
+            answer[i] = count;
+        }
+
+        
+        return answer;
+    }
+}
+*/
