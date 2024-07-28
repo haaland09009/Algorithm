8aa4e63ce7ea77595dfc85279da23ba3f605f79e
09^_^ <87968927+haaland09009@users.noreply.github.com>
2024-07-28 16:53:21 +0900

    20240728

---
+++ Programmers/Level 1/정수 제곱근 판별/Solution.java
@@ -0,0 +1,10 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12934
+class Solution {
+    public long solution(long n) {
+
+        if (Math.pow((int) Math.sqrt(n), 2) == n) {
+            return (long) Math.pow(Math.sqrt(n) + 1, 2);
+        }
+        return -1;
+    }
+}
